10104ef88111b9e3417aea3cd46e9d4a6e860a85
Superloup10 <Superloup10@gmail.com>
2013-08-20 20:05:29 +0200

    Update NPItemList.java

--- nether_plus/common/item/NPItemList.java
+++ nether_plus/common/item/NPItemList.java
@@ -7,10 +7,12 @@ public class NPItemList
 {
 	public static Item CharoiteCrystal;
 	public static Item BlackIronIngot;
+	public static Item BlockBone;
 	
 	public static void loadItem()
 	{
 		CharoiteCrystal = new CharoiteCrystal(NPProperties.CharoiteCrystalID).setUnlocalizedName("CharoiteCrystal");
 		BlackIronIngot = new BlackIronIngot(NPProperties.BlackIronIngotID).setUnlocalizedName("BlackIronIngot");
+		BlackBone = new BlackBone(NPProperties.BlackBoneID).setUnlocalizedName("BlackBone");
 	}
 }
