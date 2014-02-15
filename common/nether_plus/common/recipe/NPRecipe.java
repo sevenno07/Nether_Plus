5e36661a6bf9e570033065eba7a90bdbc6ec8099
Superloup10 <Superloup10@gmail.com>
2014-02-15 17:47:05 +0100

    fix recipes for 1.7.2

--- common/nether_plus/common/recipe/NPRecipe.java
+++ common/nether_plus/common/recipe/NPRecipe.java
@@ -1,7 +1,7 @@
 package nether_plus.common.recipe;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import nether_plus.common.block.NPBlockList;
 import nether_plus.common.item.NPItemList;
@@ -11,17 +11,17 @@ public class NPRecipe
 {
 	public static void loadRecipe()
 	{
-		GameRegistry.addRecipe(new ItemStack(NPBlockList.GlowstoneSand, 1), new Object[]{"XX", "XX", 'X', Item.glowstone});
-		GameRegistry.addRecipe(new ItemStack(NPItemList.SkeletonWhiteHelmet, 1), new Object[]{"XXX", "X X", 'X', Item.bone});
-		GameRegistry.addRecipe(new ItemStack(NPItemList.SkeletonWhiteChestplate, 1), new Object[]{"X X", "XXX", "XXX", 'X', Item.bone});
-		GameRegistry.addRecipe(new ItemStack(NPItemList.SkeletonWhiteLeggings, 1), new Object[]{"XXX", "X X", "X X", 'X', Item.bone});
-		GameRegistry.addRecipe(new ItemStack(NPItemList.SkeletonWhiteBoot, 1), new Object[]{"X X", "X X", 'X', Item.bone});
+		GameRegistry.addRecipe(new ItemStack(NPBlockList.GlowstoneSand, 1), new Object[]{"XX", "XX", 'X', Items.glowstone_dust});
+		GameRegistry.addRecipe(new ItemStack(NPItemList.SkeletonWhiteHelmet, 1), new Object[]{"XXX", "X X", 'X', Items.bone});
+		GameRegistry.addRecipe(new ItemStack(NPItemList.SkeletonWhiteChestplate, 1), new Object[]{"X X", "XXX", "XXX", 'X', Items.bone});
+		GameRegistry.addRecipe(new ItemStack(NPItemList.SkeletonWhiteLeggings, 1), new Object[]{"XXX", "X X", "X X", 'X', Items.bone});
+		GameRegistry.addRecipe(new ItemStack(NPItemList.SkeletonWhiteBoot, 1), new Object[]{"X X", "X X", 'X', Items.bone});
 		GameRegistry.addRecipe(new ItemStack(NPItemList.SkeletonBlackHelmet, 1), new Object[]{"XXX", "X X", 'X', NPItemList.BlackBone});
 		GameRegistry.addRecipe(new ItemStack(NPItemList.SkeletonBlackChestplate, 1), new Object[]{"X X", "XXX", "XXX", 'X', NPItemList.BlackBone});
 		GameRegistry.addRecipe(new ItemStack(NPItemList.SkeletonBlackLeggings, 1), new Object[]{"XXX", "X X", "X X", 'X', NPItemList.BlackBone});
 		GameRegistry.addRecipe(new ItemStack(NPItemList.SkeletonBlackBoot, 1), new Object[]{"X X", "X X", 'X', NPItemList.BlackBone});
 		GameRegistry.addShapelessRecipe(new ItemStack(NPItemList.BlackBoneMeal, 3), new Object[]{NPItemList.BlackBone});
-		GameRegistry.addRecipe(new ItemStack(NPBlockList.fourOff, 1), new Object[]{"XXX", "X X", "YYY", 'X', NPBlockList.CorruptedBrick, 'Y', Block.obsidian});
+		GameRegistry.addRecipe(new ItemStack(NPBlockList.fourOff, 1), new Object[]{"XXX", "X X", "YYY", 'X', NPBlockList.CorruptedBrick, 'Y', Blocks.obsidian});
 		GameRegistry.addShapelessRecipe(new ItemStack(NPBlockList.GrimwoodPlanks, 4), new Object[]{NPBlockList.GrimwoodLog});
 		GameRegistry.addRecipe(new ItemStack(NPBlockList.GrimwoodWorkbench, 1), new Object[]{"XX", "XX", 'X', NPBlockList.GrimwoodPlanks});
 		GameRegistry.addRecipe(new ItemStack(NPItemList.GrimwoodStick, 4), new Object[]{"X", "X", 'X', NPBlockList.GrimwoodPlanks});
@@ -66,21 +66,21 @@ public class NPRecipe
 		GameRegistry.addRecipe(new ItemStack(NPItemList.SalamanderBoot, 1), new Object[]{"X X", "X X", 'X', NPItemList.SalamanderLeather});
 		GameRegistry.addRecipe(new ItemStack(NPItemList.ChiliSeed, 1), new Object[]{"X", 'X', NPItemList.Chili});
 		GameRegistry.addRecipe(new ItemStack(NPBlockList.ChiliB, 1), new Object[]{"XXX", "XXX", "XXX", 'X', NPItemList.Chili});
-		GameRegistry.addRecipe(new ItemStack(NPBlockList.GrimwoodTorch, 4), new Object[]{"X", "Y",  'X', Item.coal, 'Y', NPItemList.GrimwoodStick});
-		GameRegistry.addRecipe(new ItemStack(NPBlockList.GrimwoodTorch, 4), new Object[]{"X", "Y",  'X', new ItemStack(Item.coal, 1, 1), 'Y', NPItemList.GrimwoodStick});
+		GameRegistry.addRecipe(new ItemStack(NPBlockList.GrimwoodTorch, 4), new Object[]{"X", "Y",  'X', Items.coal, 'Y', NPItemList.GrimwoodStick});
+		GameRegistry.addRecipe(new ItemStack(NPBlockList.GrimwoodTorch, 4), new Object[]{"X", "Y",  'X', new ItemStack(Items.coal, 1, 1), 'Y', NPItemList.GrimwoodStick});
 		GameRegistry.addRecipe(new ItemStack(NPBlockList.Ladder, 3), new Object[]{"X X", "XXX", "X X", 'X', NPItemList.GrimwoodStick});
-		GameRegistry.addRecipe(new ItemStack(NPItemList.GoldBucket, 1), new Object[]{"X X", " X ", 'X', Item.ingotGold});
+		GameRegistry.addRecipe(new ItemStack(NPItemList.GoldBucket, 1), new Object[]{"X X", " X ", 'X', Items.gold_ingot});
 		GameRegistry.addRecipe(new ItemStack(NPBlockList.SoulGlassPane, 16), new Object[]{"XXX", "XXX", 'X', NPBlockList.SoulGlass});
 	}
 	
 	public static void loadSmelting()
 	{
-		GameRegistry.addSmelting(Block.slowSand.blockID, new ItemStack(NPBlockList.SoulGlass), 2.0F);
-		GameRegistry.addSmelting(NPBlockList.GlowstoneSand.blockID, new ItemStack(Block.glowStone), 2.0F);
-		GameRegistry.addSmelting(NPBlockList.CorruptedGoldOre.blockID, new ItemStack(Item.ingotGold), 2.0F);
-		GameRegistry.addSmelting(NPBlockList.CorruptedBlackIronOre.blockID, new ItemStack(NPItemList.BlackIronIngot), 2.0F);
-		GameRegistry.addSmelting(NPBlockList.CorruptedCharoiteOre.blockID, new ItemStack(NPItemList.CharoiteCrystal), 2.0F);
-		GameRegistry.addSmelting(NPBlockList.CorruptionStone.blockID, new ItemStack(NPBlockList.CorruptedCobblestone), 2.0F);
-		GameRegistry.addSmelting(NPBlockList.InfernumOre.blockID, new ItemStack(NPItemList.InfernumIngot), 2.0F);
+		GameRegistry.addSmelting(Blocks.soul_sand, new ItemStack(NPBlockList.SoulGlass), 2.0F);
+		GameRegistry.addSmelting(NPBlockList.GlowstoneSand, new ItemStack(Blocks.glowstone), 2.0F);
+		GameRegistry.addSmelting(NPBlockList.CorruptedGoldOre, new ItemStack(Items.gold_ingot), 2.0F);
+		GameRegistry.addSmelting(NPBlockList.CorruptedBlackIronOre, new ItemStack(NPItemList.BlackIronIngot), 2.0F);
+		GameRegistry.addSmelting(NPBlockList.CorruptedCharoiteOre, new ItemStack(NPItemList.CharoiteCrystal), 2.0F);
+		GameRegistry.addSmelting(NPBlockList.CorruptionStone, new ItemStack(NPBlockList.CorruptedCobblestone), 2.0F);
+		GameRegistry.addSmelting(NPBlockList.InfernumOre, new ItemStack(NPItemList.InfernumIngot), 2.0F);
 	}
 }
