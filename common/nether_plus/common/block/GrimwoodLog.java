5bdf3da66b757de8c5f8b7fe1aaabecc73b832a0
Superloup10 <Superloup10@gmail.com>
2014-02-16 17:04:21 +0100

    fix GrimwoodLog for 1.7.2

--- common/nether_plus/common/block/GrimwoodLog.java
+++ common/nether_plus/common/block/GrimwoodLog.java
@@ -6,9 +6,9 @@ import java.util.Random;
 import net.minecraft.block.BlockLog;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
-import net.minecraft.world.World;
 import nether_plus.common.creativetabs.NetherPlusCreativeTabs;
 
 public class GrimwoodLog extends BlockLog
@@ -21,9 +21,9 @@ public class GrimwoodLog extends BlockLog
 		this.setCreativeTab(NetherPlusCreativeTabs.NPCreativeTabsBlock);
 	}
 
-	public int idDropped(int side, Random random, int par3)
+	public Item idDropped(int side, Random random, int par3)
 	{
-		return this.blockID;
+		return Item.getItemFromBlock(this);
 	}
 	
 	public void registerIcons(IIconRegister iconregister)
@@ -38,20 +38,8 @@ public class GrimwoodLog extends BlockLog
 		return k == 0 && (side == 1 || side == 0) ? topIcon : (k == 4 && (side == 5 || side == 4) ? topIcon : (k == 8 && (side == 2 || side == 3) ? topIcon : blockIcon));
 	}
 
-	public void getSubBlocks(int id, CreativeTabs creativeTabs, List list)
+	public void getSubBlocks(Item item, CreativeTabs creativeTabs, List list)
 	{
-		list.add(new ItemStack(id, 1, 0));
-	}
-
-	@Override
-	public boolean canSustainLeaves(World world, int x, int y, int z)
-	{
-		return true;
-	}
-
-	@Override
-	public boolean isWood(World world, int x, int y, int z)
-	{
-		return true;
+		list.add(new ItemStack(item, 1, 0));
 	}
 }
