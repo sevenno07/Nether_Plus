c52adc0cad0794be84e204bed2503e978d753288
Superloup10 <Superloup10@gmail.com>
2014-02-16 17:06:46 +0100

    fix GrimwoodSapling for 1.7.2

--- common/nether_plus/common/block/GrimwoodSapling.java
+++ common/nether_plus/common/block/GrimwoodSapling.java
@@ -6,12 +6,12 @@ import java.util.Random;
 import net.minecraft.block.BlockSapling;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import nether_plus.common.GrimGenTree;
-import nether_plus.common.config.NPProperties;
 import nether_plus.common.creativetabs.NetherPlusCreativeTabs;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -40,25 +40,19 @@ public class GrimwoodSapling extends BlockSapling
 	public void growTree(World world, int i, int j, int k, Random random)
 	{
 		int l = world.getBlockMetadata(i, j, k) & 3;
-		world.setBlock(i, j, k, 0);
+		world.setBlock(i, j, k, this, 0, l);
 		Object obj = null;
 		obj = new GrimGenTree(false);
 		if(!((WorldGenerator)(obj)).generate(world, random, i, j, k))
 		{
-			world.setBlock(i, j, k, blockID, l, 3);
+			world.setBlock(i, j, k, this, l, 3);
 		}
 	}
 
-	@Override
-	protected boolean canThisPlantGrowOnThisBlockID(int blockid)
-	{
-		return blockid == NPProperties.CorruptionStoneID;
-	}
-
 	@SideOnly(Side.CLIENT)
-	public void getSubBlocks(int blockid, CreativeTabs creativetabs, List list)
+	public void getSubBlocks(Item item, CreativeTabs creativetabs, List list)
 	{
-		list.add(new ItemStack(blockid, 1, 0));
+		list.add(new ItemStack(item, 1, 0));
 	}
 
 	public void fertilize(World world, int x, int y, int z)
