3e762e84dd6f84752fb88370919806989571f931
Superloup10 <Superloup10@gmail.com>
2014-02-16 15:51:14 +0100

    fix GrimwoodWorkbench for 1.7.2

--- common/nether_plus/common/block/GrimwoodWorkbench.java
+++ common/nether_plus/common/block/GrimwoodWorkbench.java
@@ -2,16 +2,17 @@ package nether_plus.common.block;
 
 import java.util.Random;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Icon;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 import nether_plus.common.Nether_plus;
 import nether_plus.common.creativetabs.NetherPlusCreativeTabs;
@@ -22,24 +23,24 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class GrimwoodWorkbench extends BlockContainer
 {
     @SideOnly(Side.CLIENT)
-    private Icon workbenchIconTop;
+    private IIcon workbenchIconTop;
     @SideOnly(Side.CLIENT)
-    private Icon workbenchIconFront;
+    private IIcon workbenchIconFront;
 
-    protected GrimwoodWorkbench(int par1)
+    protected GrimwoodWorkbench()
     {
-        super(par1, Material.wood);
+        super(Material.wood);
         this.setCreativeTab(NetherPlusCreativeTabs.NPCreativeTabsBlock);
     }
 
     @SideOnly(Side.CLIENT)
-    public Icon getIcon(int par1, int par2)
+    public IIcon getIcon(int par1, int par2)
     {
-        return par1 == 1 ? this.workbenchIconTop : (par1 == 0 ? NPBlockList.GrimwoodPlanks.getBlockTextureFromSide(par1) : (par1 != 2 && par1 != 4 ? this.blockIcon : this.workbenchIconFront));
+        return par1 == 1 ? this.workbenchIconTop : (par1 == 0 ? NPBlockList.NetherPlanks.getBlockTextureFromSide(par1) : (par1 != 2 && par1 != 4 ? this.blockIcon : this.workbenchIconFront));
     }
 	
 	@SideOnly(Side.CLIENT)
-    public void registerIcons(IconRegister par1IconRegister)
+    public void registerIcons(IIconRegister par1IconRegister)
     {
         this.blockIcon = par1IconRegister.registerIcon("Nether_plus:workbench_side");
         this.workbenchIconTop = par1IconRegister.registerIcon("Nether_plus:workbench_top");
@@ -49,7 +50,7 @@ public class GrimwoodWorkbench extends BlockContainer
 	@Override
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int i, float f, float g, float t)
 	{
-		TileEntityGrimwoodWorkbench tileentityworkbench = (TileEntityGrimwoodWorkbench) world.getBlockTileEntity(x, y, z);
+		TileEntityGrimwoodWorkbench tileentityworkbench = (TileEntityGrimwoodWorkbench) world.getTileEntity(x, y, z);
 		if(tileentityworkbench == null || player.isSneaking())
 		{
 			return false;
@@ -59,16 +60,16 @@ public class GrimwoodWorkbench extends BlockContainer
 	}
 	
 	@Override
-	public void breakBlock(World world, int x, int y, int z, int i, int j)
+	public void breakBlock(World world, int x, int y, int z, Block block, int j)
 	{
 		dropItems(world, x, y, z);
-		super.breakBlock(world, x, y, z, i, j);
+		super.breakBlock(world, x, y, z, block, j);
 	}
 	
 	private void dropItems(World world, int x, int y, int z)
 	{
 		Random rand = new Random();
-		TileEntityGrimwoodWorkbench tileentityworkbench = (TileEntityGrimwoodWorkbench) world.getBlockTileEntity(x, y, z);
+		TileEntityGrimwoodWorkbench tileentityworkbench = (TileEntityGrimwoodWorkbench) world.getTileEntity(x, y, z);
 		if(!(tileentityworkbench instanceof IInventory))
 		{
 			return;
@@ -82,7 +83,7 @@ public class GrimwoodWorkbench extends BlockContainer
 				float rx = rand.nextFloat() * 0.6F + 0.1F;
 				float ry = rand.nextFloat() * 0.6F + 0.1F;
 				float rz = rand.nextFloat() * 0.6F + 0.1F;
-				EntityItem entity_item = new EntityItem(world, x + rx, y + ry, z + rz, new ItemStack(item.itemID, item.stackSize, item.getItemDamage()));
+				EntityItem entity_item = new EntityItem(world, x + rx, y + ry, z + rz, new ItemStack(item.getItem(), item.stackSize, item.getItemDamage()));
 				if(item.hasTagCompound())
 				{
 					entity_item.getEntityItem().setTagCompound((NBTTagCompound) item.getTagCompound().copy());
@@ -98,7 +99,7 @@ public class GrimwoodWorkbench extends BlockContainer
 	}
 	
 	@Override
-	public TileEntity createNewTileEntity(World world)
+	public TileEntity createNewTileEntity(World world, int i)
 	{
 		return new TileEntityGrimwoodWorkbench();
 	}
