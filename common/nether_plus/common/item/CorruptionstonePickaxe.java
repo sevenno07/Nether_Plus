f2a68773aeac5c74321cabec91bb096b997b101a
sevenno07 <sevenno@hotmail.fr>
2014-02-19 13:52:36 +0100

    ajour tes outils en coruptionstone (codes)

---
+++ common/nether_plus/common/item/CorruptionstonePickaxe.java
@@ -0,0 +1,28 @@
+package nether_plus.common.item;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemPickaxe;
+import net.minecraft.item.ItemStack;
+import nether_plus.common.creativetabs.NetherPlusCreativeTabs;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class CorruptionstonePickaxe extends ItemPickaxe
+{
+	public CorruptionstonePickaxe(ToolMaterial toolmaterial)
+	{
+		super(toolmaterial);
+		this.setCreativeTab(NetherPlusCreativeTabs.NPCreativeTabsTool);
+	}
+	
+	public boolean getIsRepairable(ItemStack stack, ItemStack outputstack)
+	{
+		return true;
+	}
+	
+	@SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iconregister)
+	{
+        this.itemIcon = iconregister.registerIcon("nether_plus:CorruptionstonePickaxe");
+ 	}
+}
